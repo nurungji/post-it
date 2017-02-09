c61395b676650d8bf256c30e9b9192700c6b8012
nickpark <nick.park1860@gmail.com>
2017-02-10 01:04:02 +0900

    Before saving str into a file, write a code to occur event for a certain moment.

--- post_it_main.py
+++ post_it_main.py
@@ -1,7 +1,34 @@
-import tkinter
+# http://pythonstudy.xyz/python/article/111-Tkinter-%EC%9D%B4%EB%B2%A4%ED%8A%B8
+#
+#
 
-root = tkinter.Tk()
-text = tkinter.Text(root)
-text.pack()
-text.insert('1.0', 'Type something!!')
-root.mainloop()
+import tkinter
+from tkinter import *
+
+
+def key_pressed(event):
+    return True
+
+
+def click_c(event):
+    return True
+
+
+def main():
+    window = tkinter.Tk()
+    window.title("post-it")
+
+    text = tkinter.Text(window)
+    text.insert("1.0", "Write a code to save text...")
+
+    if text.bind("<Button-1>", click_c):
+        print("hello")
+        print(str(text.get("1.0", END)))
+
+    # text.bind("<Key>", key_pressed)
+    text.pack()
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
